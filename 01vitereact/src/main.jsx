558ea222cbda19476c63bd95cf81aad2b4824879
Panvir <panvirsingh2002@gmail.com>
2026-04-18 00:09:30 +0530

    custom react and inbuilt Reactelement fxn learning

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+function Myapp(){
+  return (
+    <>
+    <h1>
+      custom react
+      </h1></>
+  )
+}
+const ReactElement = {
+    type: 'a',
+    props:{
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children:'Click me to visit google'
+}
+const AnotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotheruser="chai aur react"
+
+const reactElement=React.createElement(
+  'a',{
+    href:'https://google.com',target:'_blank'
+  },
+  'click me to visit google',
+  AnotherElement
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
 )
