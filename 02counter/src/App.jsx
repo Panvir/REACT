fd9d0f8a5dda29fe75f961ed92ca6ae94249366e
Panvir <panvirsingh2002@gmail.com>
2026-04-19 16:33:32 +0530

    BgColorChanger

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,18 +2,22 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-  let [counter,setCounter] = useState(0)//setCountr ipdae kreha counter(varibale hai ye) ko 25 is initial value
+  const [counter,setCounter] = useState(15)//setCountr ipdae kreha counter(varibale hai ye) ko 25 is initial value
   //ue state retur 2 thnkgs at 0th index varibale and 1st index fxn is coller
 
   const removeValue=()=>{
-    if(counter>0)
+    // if(counter>0)
       setCounter(counter-1);
     
   }
   const addvalue=()=>{
     // console.log(`${counter}`);
-    if(counter<20)
-    setCounter(counter+1)
+    // if(counter<20)
+    setCounter(prev=> prev+1)//pehle prev value ayi prev ch then add kro 
+    setCounter(prev=> prev+1)
+    setCounter(prev=> prev+1)
+    setCounter(prev=> prev+1)
+      
   }
   return (
     <>
