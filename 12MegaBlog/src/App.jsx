ba9842aab7bb32d578e63117ef3b125ddf5dd4e9
Panvir <panvirsingh2002@gmail.com>
2026-05-05 19:06:30 +0530

    env setup and appwrite initialized

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  console.log();
+  
+
+  return (
+    <>
+    <h1>A blog app using appwrite</h1>
+    </>
+  )
+}
+
+export default App
